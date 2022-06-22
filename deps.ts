a4bea251a168bc2967b12fb3265452d58b74db28
zoubingwu <zoubingwu@gmail.com>
2022-06-22 22:57:33 +0800

    chore: export all from deps.ts

--- deps.ts
+++ deps.ts
@@ -1,32 +1,4 @@
-export {
-  Application,
-  Context,
-  Router,
-  Status,
-} from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export type {
-  PullRequest,
-  PullRequestAssignedEvent,
-  PullRequestClosedEvent,
-  PullRequestEvent,
-  PullRequestOpenedEvent,
-  PullRequestReopenedEvent,
-  PullRequestReviewEvent,
-  PullRequestReviewRequestedEvent,
-  PullRequestReviewRequestRemovedEvent,
-  PullRequestReviewSubmittedEvent,
-  PullRequestUnassignedEvent,
-  Repository,
-  Team,
-  User,
-  WebhookEvent,
-  WebhookEventMap,
-  WebhookEventName,
-} from "https://cdn.skypack.dev/-/@octokit/webhooks-types@v5.8.0-uWO0hY51WP62T6GJlTu3/dist=es2019,mode=raw/schema.d.ts";
-export { verify } from "https://cdn.skypack.dev/@octokit/webhooks-methods";
-export {
-  bold,
-  cyan,
-  green,
-  yellow,
-} from "https://deno.land/std@0.140.0/fmt/colors.ts";
+export * from "https://deno.land/x/oak@v10.6.0/mod.ts";
+export * from "https://cdn.skypack.dev/-/@octokit/webhooks-types@v5.8.0-uWO0hY51WP62T6GJlTu3/dist=es2019,mode=raw/schema.d.ts";
+export * from "https://cdn.skypack.dev/@octokit/webhooks-methods";
+export * from "https://deno.land/std@0.140.0/fmt/colors.ts";
